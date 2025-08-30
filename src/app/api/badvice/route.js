36a73906a8d51b431b913ac194ec6e966a325f49
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2025-08-30 07:53:32 +0000

    Added rate limit

--- src/app/api/badvice/route.js
+++ src/app/api/badvice/route.js
@@ -1,8 +1,29 @@
 import { generateText } from "ai";
 import { groq } from "@ai-sdk/groq";
+import { RateLimiterMemory } from "rate-limiter-flexible";
+
+const rateLimiter = new RateLimiterMemory({
+  points: 1,
+  duration: 5,
+});
 
 export async function POST(request) {
   try {
+    const ip =
+      request.headers.get("cf-connecting-ip")?.trim() ??
+      request.headers.get("x-forwarded-for")?.split(",")[0]?.trim() ??
+      request.headers.get("x-real-ip")?.trim() ??
+      "127.0.0.1";
+
+    try {
+      await rateLimiter.consume(ip, 1);
+    } catch (error) {
+      return Response.json(
+        { error: "Wait 5 seconds before you send another request!" },
+        { status: 429 }
+      );
+    }
+
     const { prompt } = await request.json();
     const result = await generateText({
       model: groq("llama-3.1-8b-instant"),
--- src/app/components/Input.jsx
+++ src/app/components/Input.jsx
@@ -5,6 +5,7 @@ import React, { useState, useRef } from "react";
 function Input() {
   const [prompt, setPrompt] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const [completion, setCompletion] = useState("");
   const inputRef = useRef(null);
 
@@ -13,8 +14,10 @@ function Input() {
   };
 
   const complete = async (e) => {
-    setLoading(true);
     e.preventDefault();
+    setCompletion("");
+    setError("");
+    setLoading(true);
     setPrompt("");
     try {
       const res = await fetch("/api/badvice", {
@@ -25,16 +28,21 @@ function Input() {
 
       if (res.ok) {
         setLoading(false);
-        const data = await res.json();
+        let data = await res.json();
         setCompletion(data.result);
+      } else {
+        setLoading(false);
+        let data = await res.json();
+        console.log(data.error);
+        setError(data.error);
       }
-    } finally {
-      console.log("HI");
+    } catch (error) {
+      console.log(error);
     }
   };
   return (
     <div className="flex flex-col gap-10">
-      <form onSubmit={complete}>
+      <form onSubmit={complete} className="flex flex-col gap-3">
         <div
           className="border-2 border-primary p-2 flex flex-col gap-4 rounded-md cursor-text"
           onClick={handleFocus}
@@ -49,17 +57,33 @@ function Input() {
           />
           <div className="flex justify-end">
             <button
-              className="bg-primary rounded-sm text-background py-1.5 px-4 w-max curosr-pointer"
+              className="bg-primary rounded-sm text-background py-1.5 px-4 w-max cursor-pointer"
               type="submit"
             >
               {loading ? "Generating..." : "Generate"}
             </button>
           </div>
         </div>
+        <div className="sm:flex flex sm:flex-row flex-col gap-3">
+          <p
+            className="cursor-pointer sm:w-[50%] w-[100%] border-accent border-2 text-center p-1 text-sm text-text"
+            onClick={() => setPrompt("How to swim in the waterpark")}
+          >
+            How to swim in the waterpark
+          </p>
+          <p
+            className="cursor-pointer sm:w-[50%] w-[100%] border-accent border-2 text-center p-1 text-sm text-text"
+            onClick={() => setPrompt("How to solve every math question")}
+          >
+            How to solve every math question
+          </p>
+        </div>
       </form>
       <div className="border-2 p-3 w-full min-h-40 border-secondary rounded-md">
         {completion ? (
           <p className="text-text text-lg">{completion}</p>
+        ) : error ? (
+          <p className="text-red-500 text-lg flex items-center justify-center min-h-33 text-center">{error}</p>
         ) : (
           <p className="flex items-center justify-center min-h-33 text-center text-text text-lg">
             Your brutally honest advice will appear here...
