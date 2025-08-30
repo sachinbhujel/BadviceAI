836a77d55619b53709f7124fb41fb66f760bff49
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2025-08-30 09:19:38 +0000

    Added Favicon And Title

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -46,10 +46,10 @@ function Navbar() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-sun-icon lucide-sun"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-sun-icon lucide-sun"
             >
               <circle cx="12" cy="12" r="4" />
               <path d="M12 2v2" />
@@ -71,10 +71,10 @@ function Navbar() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-moon-icon lucide-moon"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-moon-icon lucide-moon"
             >
               <path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401" />
             </svg>
