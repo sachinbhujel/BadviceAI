454c61ec1ce3c2adb47fd8203f1274427de18ac9
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2025-08-30 08:28:18 +0000

    Added theme toggler

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 function Navbar() {
+  const [theme, setTheme] = useState("light");
+
+  const handleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
   return (
-    <div className="w-full flex justify-end">
+    <div className="w-full flex justify-end gap-2">
       <a href="https://github.com/sachinbhujel/badviceAI" target="_blank">
-        <div className="p-1 rounded-full border-text border-[2px] hover:bg-secondary cursor-pointer ">
+        <div className="p-1 rounded-full border-text border-[2px] hover:bg-secondary cursor-pointer">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="20"
@@ -22,6 +36,51 @@ function Navbar() {
           </svg>
         </div>
       </a>
+      <button onClick={handleTheme} className="">
+        {theme === "light" ? (
+          <div className="p-1 rounded-full border-text border-[2px] hover:bg-secondary cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-sun-icon lucide-sun"
+            >
+              <circle cx="12" cy="12" r="4" />
+              <path d="M12 2v2" />
+              <path d="M12 20v2" />
+              <path d="m4.93 4.93 1.41 1.41" />
+              <path d="m17.66 17.66 1.41 1.41" />
+              <path d="M2 12h2" />
+              <path d="M20 12h2" />
+              <path d="m6.34 17.66-1.41 1.41" />
+              <path d="m19.07 4.93-1.41 1.41" />
+            </svg>
+          </div>
+        ) : (
+          <div className="p-1 rounded-full border-text border-[2px] hover:bg-secondary cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-moon-icon lucide-moon"
+            >
+              <path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401" />
+            </svg>
+          </div>
+        )}
+      </button>
     </div>
   );
 }
